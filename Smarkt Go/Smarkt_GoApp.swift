ecc57f2ad01022fe6a7fdfa06449d4b6a5ba6f23
Zied Dahmani <zied.dahmani2@gmail.com>
2023-03-15 00:15:17 +0100

    Sign In Screen ( UI init )

--- Smarkt Go/Smarkt_GoApp.swift
+++ Smarkt Go/Smarkt_GoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Smarkt_GoApp: App {
     var body: some Scene {
         WindowGroup {
-            MainScreen()
+            SignInScreen()
         }
     }
 }
--- Smarkt Go/Views/SignInScreen.swift
+++ Smarkt Go/Views/SignInScreen.swift
@@ -12,34 +12,94 @@ import SwiftUI
 
 struct SignInScreen: View {
     
-    @State private var username: String = ""
-    @FocusState private var emailFieldIsFocused: Bool
+    @State private var phoneNumber = ""
+
     
     var body: some View {
         
         VStack(alignment: .center) {
             
-            TextField("Phone Number", text: $username)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Image("shopping_app")
+                        .resizable()
+                        .frame(width: UIScreen.main.bounds.width / 1.5  , height: UIScreen.main.bounds.height / 4)
+                       .aspectRatio(contentMode: .fit)
+                       .padding()
+            
+            
+            TextField("Phone number", text: $phoneNumber)
+                .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
+                .frame(width: UIScreen.main.bounds.width - 40  , height: 50)
+                      .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .strokeBorder(Color.accentColor, lineWidth: 1)
+                                  )
+                      .padding()
+            
+            Button(action: {
+                print("ca marche")
+            }) {
+                HStack {
+                    Image(systemName: "phone")
+                        .foregroundColor(.white)
+                    Text("Sign in with phone")
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                }
                 .padding()
-            
-            
-            Button {
+                .frame(width: UIScreen.main.bounds.width - 40  , height: 50)
+                .background(Color.accentColor)
+                .cornerRadius(10)
+                .shadow(radius: 5)
                 
-            } label: {
-                Text("Sign in")
-                    .frame(maxWidth: .infinity)
-                    .foregroundColor(.white)
-            }
-            .frame(height: 50)
-            .background(Color.accentColor)
-            .clipShape(Capsule())
-            .padding([.horizontal,.bottom],30)
+            }.padding()
 
             
-            Divider().padding(.horizontal,40)
+            Divider()
+                .frame(height: 2)
+                .background(Color.gray)
+                .padding(.horizontal,40)
 
-  	
+            Button(action: {
+                        // Action when button is pressed
+                    }) {
+                        HStack {
+                            Image("google")
+                                       .resizable()
+                                       .aspectRatio(contentMode: .fit)
+                            Text("Sign in with Google")
+                                .fontWeight(.semibold)
+                                .foregroundColor(.black)
+                        }
+                        .padding()
+                        .frame(width: UIScreen.main.bounds.width - 40  , height: 50)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+
+                    }
+                    .padding()
+            
+            
+            Button(action: {
+                        // Action when button is pressed
+                    }) {
+                        HStack {
+                            Image(systemName: "applelogo")
+                                .foregroundColor(.white)
+                            Text("Sign in with Apple")
+                                .fontWeight(.semibold)
+                                .foregroundColor(.white)
+                        }
+                        .padding()
+                        .frame(width: UIScreen.main.bounds.width - 40  , height: 50)
+                        .background(Color.black)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+
+                    }
+
+            Spacer()
+
             
         }.padding()
 
