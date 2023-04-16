d44c24162c5cf03a2c9cd3e2c402e4339fd77952
Mohamed Islem Samaali <74082867+myrmn@users.noreply.github.com>
2023-04-16 17:02:43 +0200

    favorite navigation

--- Smarkt Go/Views/FavoritesScreen.swift
+++ Smarkt Go/Views/FavoritesScreen.swift
@@ -8,33 +8,39 @@
 import SwiftUI
 
 struct FavoritesScreen: View {
-    @ObservedObject var favoritesViewModel = FavoritesViewModel()
-
+    @StateObject var favoritesViewModel = FavoritesViewModel()
+    
     var body: some View {
-        if favoritesViewModel.favorites.isEmpty {
-            VStack(alignment: .center) {
-                Image("nofavorites")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: UIScreen.main.bounds.width - 40, height:UIScreen.main.bounds.height / 3)
-                
-                Text(Strings.knoFavorites)
-                    .fontWeight(.semibold)
-                    .font(.subheadline)
-                    .foregroundColor(Color.gray)
-                    .padding(.top, Constants.ksmallSpace)
-            }
-        }
-        else {
-            ScrollView {
-                LazyVStack {
-                    ForEach(favoritesViewModel.favorites) { favorite in
-                        FavoriteItem(imageName: Constants.kbaseUrl+favorite.images[0], text1: favorite.name, text2: favorite.description)
-                                .padding(.vertical, 10)
+        NavigationView {
+            if favoritesViewModel.favorites.isEmpty {
+                VStack(alignment: .center) {
+                    Image("nofavorites")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: UIScreen.main.bounds.width - 40, height:UIScreen.main.bounds.height / 3)
                     
-                    }}
+                    Text(Strings.knoFavorites)
+                        .fontWeight(.semibold)
+                        .font(.subheadline)
+                        .foregroundColor(Color.gray)
+                        .padding(.top, Constants.ksmallSpace)
+                }
+            } else {
+                ScrollView (showsIndicators: false){
+                    VStack  {
+                        ForEach(favoritesViewModel.favorites) { favorite in
+                            NavigationLink(destination: SupermarketScreen(supermarket: favorite)
+                            )
+                            {
+                                FavoriteItem(imageName: Constants.kbaseUrl+favorite.images[0], text1: favorite.name, text2: favorite.description)
+                                    .padding(.vertical, 10)
+                            }
+                        }
+                    }
+                }
+                
+                
             }
-        
         }
     }
 }
